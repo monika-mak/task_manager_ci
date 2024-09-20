eb4d8b24b6ebae21f4aa6dc5d75d79fb9345c4fa
Monika Mak <monika1986mak@gmail.com>
2024-09-20 10:53:00 +0000

    add nav bar and footer  to to the page

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,1 +1,6 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    //sidenav initalization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
